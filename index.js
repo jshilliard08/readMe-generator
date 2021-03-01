66406978d3e863d3f9250ef0d30fd25779997c0a
jshilliard08 <73717438+jshilliard08@users.noreply.github.com>
2021-03-01 09:32:32 -0500

    adding questions

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
+const axios = require("axios");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "badge",
+        message: "Please provide the badge links that you want."
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide a description for your project."
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please provide the installation instructions."
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please provide the project's usage."
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "Please provide the project license or your badge link."
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please provide the contributing parties."
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please provide the project tests."
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your github user name?"
+    },
+    {
+        type: "input",
+        name: "repo",
+        message: "Please provide your repo link."
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
